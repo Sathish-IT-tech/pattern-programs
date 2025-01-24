3f8cc524da00e603cfe0e034b00d3ae9ef756297
Sathish-IT-tech <sathishbtechit2004@gmail.com>
2025-01-24 12:03:28 +0530

    Create RightAngledTriangle2.java

---
+++ RightAngledTriangle2.java
@@ -0,0 +1,29 @@
+// Program for Dynamic patterns
+// This like to print 1 and 0 in right angled triangle
+// Expected output is
+// 1
+// 0 1
+// 1 0 1
+// 0 1 0 1
+// 1 0 1 0 1
+
+import java.util.Scanner;
+class RightAngledTriangle2{
+  public static void main(String[]args){
+    Scanner scan=new Scanner(System.in);
+    System.out.println("Enter the number of rows you want to print");
+    int n=scan.nextInt();
+    for(int i =1;i<=n;i++){
+       for(int j=1;j<=i;j++){
+           if((i+j)%2==0){
+               System.out.print("1");
+           }
+           else{
+               System.out.print("0");
+           } 
+           
+       }
+      System.out.println("");
+  }
+}
+}
